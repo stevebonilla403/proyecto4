a1be517ffb605dbd83644b7a9f9abab963f51f83
stevebonilla403 <111099095+stevebonilla403@users.noreply.github.com>
2022-08-21 20:35:47 -0600

    Add files via upload

---
+++ semana4-primer-proyecto-formulario/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JUEGODEPAREJAS4SEMANA
+{
+    public partial class Form1 : Form
+
+
+
+
+    {
+       
+        Label firstClicked = null;
+
+      
+
+        Label secondClicked = null;
+
+
+        Random random = new Random();
+
+        // cada una de las letras es un icono interesante 
+        // y cada icono aparece dos veces en esta lista
+
+
+        List<string> icons = new List<string>()
+    {
+        "A","A","G","G","S","S","P","P",
+        "M","M","Q","Q","Ñ","Ñ","O","O"
+    };
+
+        /// <resumen>
+        /// Asigna cada icono de la lista de iconos a un cuadrado alaeatorio
+        /// </resumen>
+
+
+        private void AssignIconsToSquares()
+        {
+            // El TableLayoutPanel tiene 16 etiquetas,
+            // Y la lista de iconos tiene 16 iconos,
+            // Por lo que se extrae un icono al azar de la lista
+            // y añadido a cada etiqueta
+
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+
+                }
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            AssignIconsToSquares();
+        }
+
+        /// <summary>
+        /// Every label's Click event is handled by this event handler
+        /// </summary>
+        /// <param name="sender">The label that was clicked</param>
+        /// <param name="e"></param>
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+               
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+               
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+               
+                timer1.Start();
+
+                
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// This timer is started when the player clicks 
+        /// two icons that don't match,
+        /// so it counts three quarters of a second 
+        /// and then turns itself off and hides both icons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            // Hide both icons
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            // Reset firstClicked and secondClicked 
+            // so the next time a label is
+            // clicked, the program knows it's the first click
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel, 
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            
+            MessageBox.Show("ENHORABUENA!!!!!!!!");
+            Close();
+        }
+
+
+
+
+    }
+}
+
+
+    
+                
+
+            
+
+            
+        
+    
+
+  
